768515f907a9f681e65ddc892c1159cc1be27298
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-28 11:40:25 -0700

    Updated to class components and set state

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 
-const ToDoForm = () => {
-  return (
-    <div className="form-container">
-      <form>
-        <input type="text" placeholder="Add to-do" />
-        <button type="submit">Add To Do</button>
-        <button type="submit">Clear Completed</button>
-      </form>
-    </div>
-  );
-};
+class ToDoForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newItem: ""
+    };
+  }
+
+  render() {
+    return (
+      <div className="form-container">
+        <form>
+          <input type="text" placeholder="Add to-do" name="item" id="item" />
+          <button type="submit">Add To Do</button>
+          <button type="submit">Clear Completed</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default ToDoForm;
