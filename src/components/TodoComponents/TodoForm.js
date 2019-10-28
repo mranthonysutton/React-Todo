6458a6a76dce71b316b714d4a27050bd92c255ac
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-28 12:29:00 -0700

    Added form to handle change and to set state when the add to do button is clicked

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -8,13 +8,37 @@ class ToDoForm extends Component {
     };
   }
 
+  handleChange = e => {
+    this.setState({ newItem: e.target.value });
+  };
+
+  handleAddTask = e => {
+    // Stops the form from refreshing
+    e.preventDefault();
+    // Clears the input to an empty value
+    this.setState({ newItem: "" });
+    // Logs the state of what was input from the form, into state
+    console.log(this.state.newItem);
+  };
+
   render() {
     return (
       <div className="form-container">
         <form>
-          <input type="text" placeholder="Add to-do" name="item" id="item" />
-          <button type="submit">Add To Do</button>
-          <button type="submit">Clear Completed</button>
+          <input
+            type="text"
+            placeholder="Add to-do"
+            name="item"
+            id="item"
+            value={this.state.newItem}
+            onChange={this.handleChange}
+          />
+          <div className="button-container">
+            <button type="submit" onClick={this.handleAddTask}>
+              Add To Do
+            </button>
+            <button type="submit">Clear Completed</button>
+          </div>
         </form>
       </div>
     );
