75d2eff60eb8b5359ddbfdaf6088366c97defac5
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-28 12:45:25 -0700

    Renders existing tasks on the screen and assigns a class to the item if it's completed state is true

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
 import TodoForm from "./components/TodoComponents/TodoForm";
+import TodoList from "./components/TodoComponents/TodoList";
+
+const data = [
+  {
+    task: "Learn Material UI",
+    id: 1,
+    completed: false
+  },
+  {
+    task: "Start Side Project",
+    id: 2,
+    completed: false
+  },
+  {
+    task: "Plan Date Night",
+    id: 3,
+    completed: false
+  },
+  {
+    task: "Plan Christmas Party",
+    id: 4,
+    completed: false
+  }
+];
 
 class App extends Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      list: data
+    };
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
         <TodoForm />
+        <TodoList task={this.state.list} />
       </div>
     );
   }
